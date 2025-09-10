2fa7ad36bfedb831a179ac988d8ba9208ebf992c
Okan <okan.unallar@gmail.com>
2025-09-10 12:05:00 +0300

    refactor: clear unnecessary files

--- src/features/user-dashboard/utils/product-category-utils.ts
+++
@@ -1,8 +0,0 @@
-interface SortByRiskyCountItem {
-  details_for_risky: { risky_count: number };
-}
-
-export const sortByRiskyCount = (
-  a: SortByRiskyCountItem,
-  b: SortByRiskyCountItem
-) => b.details_for_risky.risky_count - a.details_for_risky.risky_count;
