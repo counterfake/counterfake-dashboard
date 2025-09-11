713e574fe11d6c617b33ae88a28937e085f012b6
Okan <okan.unallar@gmail.com>
2025-09-11 21:30:07 +0300

    feat: add new soft variants and size property to badge component

--- src/common/components/ui/primitives/badge.tsx
+++ src/common/components/ui/primitives/badge.tsx
@@ -4,26 +4,32 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/common/lib/utils/ui";
 
 const badgeVariants = cva(
-  "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-medium transition-colors focus:outline-hidden focus:ring-2 focus:ring-ring focus:ring-offset-2",
+  "inline-flex items-center rounded-full border px-2.5 py-0.5 font-medium",
   {
     variants: {
       variant: {
-        default:
-          "border-transparent bg-primary text-primary-foreground hover:bg-primary/80",
-        secondary:
-          "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        destructive:
-          "border-destructive/50 bg-destructive/10 text-destructive [&>svg]:text-destructive",
-        outline: "text-foreground",
-        success:
+        default: "border-muted-foreground/10 text-muted-foreground bg-muted",
+        primarySoft: "border-primary/10 text-primary bg-primary/10",
+        secondarySoft:
+          "border-secondary bg-secondary/10 text-secondary-foreground",
+        destructiveSoft:
+          "border-destructive/30 bg-destructive/10 text-destructive [&>svg]:text-destructive",
+        successSoft:
           "border-success/50 bg-success/10 text-success [&>svg]:text-success",
-        warning:
+        warningSoft:
           "border-warning/50 bg-warning/10 text-warning [&>svg]:text-warning",
-        info: "border-info/50 bg-info/10 text-info [&>svg]:text-info",
+        infoSoft: "border-info/50 bg-info/10 text-info [&>svg]:text-info",
+        outline: "text-foreground",
+      },
+      size: {
+        default: "text-xs",
+        lg: "text-sm",
+        xl: "text-base",
       },
     },
     defaultVariants: {
       variant: "default",
+      size: "default",
     },
   }
 );
@@ -32,9 +38,12 @@ export interface BadgeProps
   extends React.HTMLAttributes<HTMLDivElement>,
     VariantProps<typeof badgeVariants> {}
 
-function Badge({ className, variant, ...props }: BadgeProps) {
+function Badge({ className, variant, size, ...props }: BadgeProps) {
   return (
-    <div className={cn(badgeVariants({ variant }), className)} {...props} />
+    <div
+      className={cn(badgeVariants({ variant, size }), className)}
+      {...props}
+    />
   );
 }
 
